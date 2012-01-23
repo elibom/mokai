dfd49895db77e0d1e55b3bf06cb718d083d3b59f
German Escobar <german.escobarc@gmail.com>
2012-01-23 17:17:04 -0500

    Create the connectors-0.5.xsd file and update references.

--- mokai-core/src/test/java/org/mokai/config/xml/test/SchemaValidationTest.java
+++ mokai-core/src/test/java/org/mokai/config/xml/test/SchemaValidationTest.java
@@ -7,26 +7,10 @@ import org.mokai.config.xml.SchemaEntityResolver;
 import org.testng.annotations.Test;
 
 public class SchemaValidationTest {
-
-	@Test
-	public void shouldLoadValidReceiversDocument() throws Exception {
-		String path = "src/test/resources/schema-test/good-receivers.xml";
-		
-		// create the document
-		SAXReader reader = new SAXReader();
-		reader.setEntityResolver(new SchemaEntityResolver());
-		reader.setValidation(true);
-		
-		reader.setFeature("http://xml.org/sax/features/validation", true);
-        reader.setFeature("http://apache.org/xml/features/validation/schema", true );
-        reader.setFeature("http://apache.org/xml/features/validation/schema-full-checking", true);
-		
-		reader.read(new File(path));
-	}
 	
 	@Test
-	public void shouldLoadValidProcessorsDocument() throws Exception {
-		String path = "src/test/resources/schema-test/good-processors.xml";
+	public void shouldLoadValidConnectorsDocument() throws Exception {
+		String path = "src/test/resources/schema-test/good-connectors.xml";
 		
 		// create the document
 		SAXReader reader = new SAXReader();

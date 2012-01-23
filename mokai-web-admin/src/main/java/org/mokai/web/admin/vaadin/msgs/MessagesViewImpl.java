715a8f53f2eaea6d91fd0b2ad91099c840a44e07
German Escobar <german.escobarc@gmail.com>
2012-01-23 09:34:04 -0500

    Issue #27: Filter messages by recipient and status.
    Add a form to the messages view that contains the recipient and the status checkboxes.
    Add the logic to filter the messages.

--- mokai-web-admin/src/main/java/org/mokai/web/admin/vaadin/msgs/MessagesViewImpl.java
+++ mokai-web-admin/src/main/java/org/mokai/web/admin/vaadin/msgs/MessagesViewImpl.java
@@ -1,6 +1,12 @@
 package org.mokai.web.admin.vaadin.msgs;
 
+import java.util.Collection;
+
+import org.mokai.Message;
+import org.mokai.Message.Direction;
 import org.mokai.RoutingEngine;
+import org.mokai.persist.MessageCriteria;
+import org.mokai.persist.MessageCriteria.OrderType;
 import org.mokai.web.admin.vaadin.WebAdminContext;
 
 import com.github.peholmst.mvp4vaadin.AbstractView;
@@ -8,9 +14,11 @@ import com.github.peholmst.mvp4vaadin.VaadinView;
 import com.vaadin.ui.Alignment;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.CheckBox;
 import com.vaadin.ui.ComponentContainer;
+import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.TextField;
 import com.vaadin.ui.VerticalLayout;
-import com.vaadin.ui.themes.BaseTheme;
 
 /**
  * 
@@ -23,6 +31,14 @@ public class MessagesViewImpl extends AbstractView<MessagesView, MessagesPresent
 	private VerticalLayout viewLayout;
 	
 	private MessagesTable messagesTable;
+	
+	private TextField recipient;
+	
+	private CheckBox sentMessages;
+	
+	private CheckBox failedMessages;
+	
+	private CheckBox unroutedMessages;
 	
 	/**
 	 * Constructor
@@ -55,16 +71,35 @@ public class MessagesViewImpl extends AbstractView<MessagesView, MessagesPresent
 		viewLayout.setMargin(true);
 		viewLayout.setSizeFull();
 		
+		HorizontalLayout searchForm = new HorizontalLayout();
+		searchForm.setWidth(420, HorizontalLayout.UNITS_PIXELS);
+		searchForm.setSpacing(true);
+		
+		// mobile field
+		recipient = new TextField();
+		recipient.setInputPrompt("Recipient");
+		searchForm.addComponent(recipient);
+		
+		sentMessages = new CheckBox("Sent", true);
+		searchForm.addComponent(sentMessages);
+		
+		failedMessages = new CheckBox("Failed", true);
+		searchForm.addComponent(failedMessages);
+		
+		unroutedMessages = new CheckBox("Unrouted", true);
+		searchForm.addComponent(unroutedMessages);
+		
 		// refresh button
 		Button refreshBtn = new Button("Refresh");
-		refreshBtn.addStyleName(BaseTheme.BUTTON_LINK);
 		refreshBtn.addListener(refreshButtonListener());
-		viewLayout.addComponent(refreshBtn);
-		viewLayout.setComponentAlignment(refreshBtn, Alignment.MIDDLE_RIGHT);
+		searchForm.addComponent(refreshBtn);
+		searchForm.setComponentAlignment(refreshBtn, Alignment.MIDDLE_RIGHT);
+		
+		viewLayout.addComponent(searchForm);
 		
 		// messages table
-		messagesTable = new MessagesTable(getRoutingEngine().getMessageStore());
-		messagesTable.loadData();
+		messagesTable = new MessagesTable();
+		messagesTable.populate(loadMessages());
 		
 		viewLayout.addComponent(messagesTable);
 		viewLayout.setExpandRatio(messagesTable, 1.0F);
@@ -76,11 +111,57 @@ public class MessagesViewImpl extends AbstractView<MessagesView, MessagesPresent
 
 			@Override
 			public void buttonClick(ClickEvent event) {
-				messagesTable.loadData();
+				
+				messagesTable.populate(loadMessages());
 			}
 		};
 	}
-
+	
+	/**
+	 * Helper method. Retrieves the messages from the database using the specified criteria.
+	 * 
+	 * @return a collection of Message objects or an empty collection if no record matches. 
+	 */
+	private Collection<Message> loadMessages() {
+		MessageCriteria criteria = new MessageCriteria()
+			.direction(Direction.TO_CONNECTIONS)
+			.orderBy("id")
+			.orderType(OrderType.DOWNWARDS)
+			.numRecords(2000);
+		
+		// add recipient criteria
+		if (recipient.getValue() != null && !"".equals(recipient.getValue())) {
+			criteria.addProperty("smsc_to", recipient.getValue());
+		}
+		
+		// add status criteria ONLY IF at least one of the checkboxes is not checked
+		boolean addStatusCriteria = !sentMessages.booleanValue() || !failedMessages.booleanValue() || !unroutedMessages.booleanValue();
+		if (addStatusCriteria) {
+			addStatusCriteria(criteria);
+		}
+		
+		return getRoutingEngine().getMessageStore().list(criteria);
+	}
+	
+	/**
+	 * Helper method. Add status criteria to the MessageCriteria object according to the selected status checkboxes.
+	 * 
+	 * @param criteria the MessageCriteria object to which we are adding the status criteria.
+	 */
+	private void addStatusCriteria(MessageCriteria criteria) {
+		if (sentMessages.booleanValue()) {
+			criteria.addStatus(Message.STATUS_PROCESSED);
+		}
+			
+		if (failedMessages.booleanValue()) {
+			criteria.addStatus(Message.STATUS_FAILED).addStatus(Message.STATUS_RETRYING);
+		}
+			
+		if (unroutedMessages.booleanValue()) {
+			criteria.addStatus(Message.STATUS_UNROUTABLE);
+		}
+	}
+	
 	@Override
 	public ComponentContainer getViewComponent() {
 		return viewLayout;

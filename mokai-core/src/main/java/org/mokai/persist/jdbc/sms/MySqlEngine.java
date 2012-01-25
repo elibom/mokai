b87ac73af5a04c3ce89ffaaed5ed0ecfbbbad0e3
German Escobar <german.escobarc@gmail.com>
2012-01-24 21:08:54 -0500

    Fix the SQL for creating the APPLICATIONS_MSGS table.
    Remove the account, sourcetype and destinationtype columns.

--- mokai-core/src/main/java/org/mokai/persist/jdbc/sms/MySqlEngine.java
+++ mokai-core/src/main/java/org/mokai/persist/jdbc/sms/MySqlEngine.java
@@ -71,12 +71,9 @@ public class MySqlEngine implements SqlEngine {
 	protected String getApplicationsCreateScript() {
 		return "CREATE TABLE " + ApplicationsSmsHandler.DEFAULT_TABLENAME + " (" +
 					"id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY, " +
-					"account VARCHAR(30), " +
 					"reference VARCHAR(100), " +
 					"source VARCHAR(30) NOT NULL, " +
-					"sourcetype TINYINT NOT NULL, " +
 					"destination VARCHAR(30), " +
-					"destinationtype TINYINT, " +
 					"status TINYINT NOT NULL, " +
 					"smsc_to VARCHAR(30), " +
 					"smsc_from VARCHAR(30), " +

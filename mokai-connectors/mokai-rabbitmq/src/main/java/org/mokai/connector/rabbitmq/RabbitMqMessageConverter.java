d3236ad91c2f99858e0df734d84100c31576250c
lariverosc <lariverosc@gmail.com>
2014-02-06 17:18:31 -0500

    Remove unused amqp properties

--- mokai-connectors/mokai-rabbitmq/src/main/java/org/mokai/connector/rabbitmq/RabbitMqMessageConverter.java
+++ mokai-connectors/mokai-rabbitmq/src/main/java/org/mokai/connector/rabbitmq/RabbitMqMessageConverter.java
@@ -16,24 +16,6 @@ public class RabbitMqMessageConverter {
         return mokaiMessage;
     }
 
-    private void setAmqpProperties(AMQP.BasicProperties basicProperties, Message mokaiMessage) {
-        if (basicProperties != null) {
-            mokaiMessage.setProperty("bp_appId", basicProperties.getAppId());
-            mokaiMessage.setProperty("bp_classId", basicProperties.getClassId());
-            mokaiMessage.setProperty("bp_className", basicProperties.getClassName());
-            mokaiMessage.setProperty("bp_clusterId", basicProperties.getClusterId());
-            mokaiMessage.setProperty("bp_contentEncoding", basicProperties.getContentEncoding());
-            mokaiMessage.setProperty("bp_contentType", basicProperties.getContentType());
-            mokaiMessage.setProperty("bp_correlationId", basicProperties.getCorrelationId());
-            mokaiMessage.setProperty("bp_deliveryMode", basicProperties.getDeliveryMode());
-            mokaiMessage.setProperty("bp_expiration", basicProperties.getExpiration());
-            mokaiMessage.setProperty("bp_messageId", basicProperties.getMessageId());
-            mokaiMessage.setProperty("bp_priority", basicProperties.getPriority());
-            mokaiMessage.setProperty("bp_timeStamp", basicProperties.getTimestamp());
-            mokaiMessage.setProperty("bp_type", basicProperties.getType());
-        }
-    }
-
     public byte[] fromMessage(Message mokaiMessage) throws UnsupportedEncodingException {
         return mokaiMessage.getProperty("body", String.class).getBytes("UTF-8");
     }

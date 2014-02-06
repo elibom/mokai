530a689a2338754b318f1b8f813ba8fb0e89e691
lariverosc <lariverosc@gmail.com>
2014-02-06 17:07:08 -0500

    Refactor Message parsing and building logic
    Add BuildJsonBodyAction which store the new properties as json within body property
    Avoid amqp specific properties in ParseJsonBodyAction

---
+++ mokai-connectors/mokai-rabbitmq/src/main/java/org/mokai/connector/rabbitmq/BuildJsonBodyAction.java
@@ -0,0 +1,28 @@
+package org.mokai.connector.rabbitmq;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonPrimitive;
+import java.util.Map;
+import org.mokai.Action;
+import org.mokai.Message;
+
+/**
+ *
+ * @author Alejandro Riveros Cruz <lariverosc@gmail.com>
+ */
+public class BuildJsonBodyAction implements Action {
+
+    @Override
+    public void execute(Message message) throws Exception {
+        Map<String, Object> properties = message.getProperties();
+        JsonObject jsonObject = new JsonObject();
+        for (Map.Entry<String, Object> property : properties.entrySet()) {
+            String key = property.getKey();
+            String value = property.getValue().toString();
+            if (!"body".equalsIgnoreCase(key)) {
+                jsonObject.add(key, new JsonPrimitive(value));
+            }
+        }
+        message.setProperty("body", jsonObject.getAsString());
+    }
+}
--- mokai-connectors/mokai-rabbitmq/src/main/java/org/mokai/connector/rabbitmq/ParseJsonBodyAction.java
+++ mokai-connectors/mokai-rabbitmq/src/main/java/org/mokai/connector/rabbitmq/ParseJsonBodyAction.java
@@ -3,7 +3,7 @@ package org.mokai.connector.rabbitmq;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
-import java.util.Map.Entry;
+import java.util.Map;
 import java.util.Set;
 import org.mokai.Action;
 import org.mokai.Message;
@@ -18,8 +18,8 @@ public class ParseJsonBodyAction implements Action {
     public void execute(Message message) throws Exception {
         String body = (String) message.getProperty("body");
         JsonObject jsonObject = new JsonParser().parse(body).getAsJsonObject();
-        Set<Entry<String, JsonElement>> jsonEntrySet = jsonObject.entrySet();
-        for (Entry<String, JsonElement> jsonEntry : jsonEntrySet) {
+        Set<Map.Entry<String, JsonElement>> jsonEntrySet = jsonObject.entrySet();
+        for (Map.Entry<String, JsonElement> jsonEntry : jsonEntrySet) {
             message.setProperty(jsonEntry.getKey(), jsonEntry.getValue().getAsString());
         }
     }

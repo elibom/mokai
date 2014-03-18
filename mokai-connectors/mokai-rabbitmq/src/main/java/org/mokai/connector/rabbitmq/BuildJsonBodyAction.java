552a55241fc26cdc5e22ce6fa9b09076776a327b
German Escobar <german.escobarc@gmail.com>
2014-03-18 17:22:37 -0500

    Use tabs instead of spaces

--- mokai-connectors/mokai-rabbitmq/src/main/java/org/mokai/connector/rabbitmq/BuildJsonBodyAction.java
+++ mokai-connectors/mokai-rabbitmq/src/main/java/org/mokai/connector/rabbitmq/BuildJsonBodyAction.java
@@ -12,17 +12,17 @@ import org.mokai.Message;
  */
 public class BuildJsonBodyAction implements Action {
 
-    @Override
-    public void execute(Message message) throws Exception {
-        Map<String, Object> properties = message.getProperties();
-        JsonObject jsonObject = new JsonObject();
-        for (Map.Entry<String, Object> property : properties.entrySet()) {
-            String key = property.getKey();
-            String value = property.getValue().toString();
-            if (!"body".equalsIgnoreCase(key)) {
-                jsonObject.add(key, new JsonPrimitive(value));
-            }
-        }
-        message.setProperty("body", jsonObject.getAsString());
-    }
+	@Override
+	public void execute(Message message) throws Exception {
+		Map<String, Object> properties = message.getProperties();
+		JsonObject jsonObject = new JsonObject();
+		for (Map.Entry<String, Object> property : properties.entrySet()) {
+			String key = property.getKey();
+			String value = property.getValue().toString();
+			if (!"body".equalsIgnoreCase(key)) {
+				jsonObject.add(key, new JsonPrimitive(value));
+			}
+		}
+		message.setProperty("body", jsonObject.getAsString());
+	}
 }

96480152e9d0f9ee4d359096a8a582c8320934c7
lariverosc <lariverosc@gmail.com>
2016-12-12 15:01:08 -0500

    Add support to retryDelay field and retry with delay

--- mokai-core/src/main/java/org/mokai/impl/camel/JesqueRedeliveryProcessor.java
+++ mokai-core/src/main/java/org/mokai/impl/camel/JesqueRedeliveryProcessor.java
@@ -32,18 +32,27 @@ public class JesqueRedeliveryProcessor implements Processor {
     public void triggerJob(String jobName, Object[] args) {
         net.greghaines.jesque.Job job = new net.greghaines.jesque.Job(jobName, args);
         jesqueClient.enqueue(jobName, job);
-        System.out.println("Succesfully enqueued job  " + jobName);
+        log.info("Succesfully enqueued job  " + jobName);
+    }
+
+    public void triggerDelayedJob(String jobName, long delay, Object[] args) {
+        net.greghaines.jesque.Job job = new net.greghaines.jesque.Job(jobName, args);
+        jesqueClient.delayedEnqueue(jobName, job, delay);
+        log.info("Succesfully enqueued job {} to be executed in {} seconds", new Object[]{jobName, delay});
     }
 
     @Override
     public void process(Exchange exchange) {
         Message message = (Message) exchange.getIn().getBody(Message.class);
         String body = (String) message.getProperty("body");
+
         JsonObject jsonMessage = new JsonParser().parse(body).getAsJsonObject();
         String deliveryToken = jsonMessage.get("deliveryToken").getAsString();
         int deliverySequence = jsonMessage.get("deliverySequence").getAsInt();
-        triggerJob(ReDeliverMessageJob, new Object[]{deliveryToken, deliverySequence});
-        log.info("Redeliver message with deliveryToken:{} and deliverySequence:{}", new Object[]{deliveryToken, deliverySequence});
+        long retryDelay = jsonMessage.get("retryDelay").getAsLong();
+        triggerDelayedJob(ReDeliverMessageJob, retryDelay, new Object[]{deliveryToken, deliverySequence});
+
+        log.info("Redeliver within {} seconds message with deliveryToken:{} and deliverySequence:{}", new Object[]{retryDelay, deliveryToken, deliverySequence});
         MessageStore messageStore = resourceRegistry.getResource(MessageStore.class);
         message.setStatus(Message.STATUS_REDELIVERED);
         messageStore.saveOrUpdate(message);
